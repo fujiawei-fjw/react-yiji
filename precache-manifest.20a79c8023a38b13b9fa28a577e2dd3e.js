80d247d10bb0ef82fc623d6f222b34bd00d9cf80
fujiawei-fjw <992572227@qq.com>
2019-09-16 11:57:20 +0800

    gh-pages.react

---
+++ precache-manifest.20a79c8023a38b13b9fa28a577e2dd3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f60c149ef933510a0c4d91cf029a19a8",
+    "url": "/react-yiji/index.html"
+  },
+  {
+    "revision": "9842ed1f1604be94f5fd",
+    "url": "/react-yiji/static/css/2.acb72abc.chunk.css"
+  },
+  {
+    "revision": "b3dc271e10b0bbacc764",
+    "url": "/react-yiji/static/css/main.52a82116.chunk.css"
+  },
+  {
+    "revision": "9842ed1f1604be94f5fd",
+    "url": "/react-yiji/static/js/2.1ffaf88e.chunk.js"
+  },
+  {
+    "revision": "b3dc271e10b0bbacc764",
+    "url": "/react-yiji/static/js/main.4d9f67a5.chunk.js"
+  },
+  {
+    "revision": "e31d103dfdddd09433ee",
+    "url": "/react-yiji/static/js/runtime~main.dabb5c1f.js"
+  }
+]);
